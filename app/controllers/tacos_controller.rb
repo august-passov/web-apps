aa61e33075e6fdd4954b6f416a191fdc39f0135e
august passov <gus.passov@kellogg.northwestern.edu>
2023-05-04 02:25:32 +0000

    Web App class practice

---
+++ app/controllers/tacos_controller.rb
@@ -0,0 +1,8 @@
+class TacosController < ApplicationController
+  def index
+    # html = "<h1>Jim is hungry</h1><p>He's really really hungry</p>"
+    # render :inline => html
+    # render :template => "tacos/index"
+  end
+end
+# action method index is responsible for handling request to tacos resource
